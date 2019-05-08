b0c914125944b60d1571e42628ed869a1a11d4ac
Vlad Ichim <ichimvlad95@gmail.com>
2019-05-08 16:52:16 +0200

    Add project files.

---
+++ TextRecognitionAndroid/TextRecognitionAndroid/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TextRecognitionAndroid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
